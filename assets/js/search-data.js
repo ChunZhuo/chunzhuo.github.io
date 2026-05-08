ee9e34f586ebfd699898455ce280343391423ffc
ChunZhuo <118121876+ChunZhuo@users.noreply.github.com>
2026-05-08 14:38:55 +0000

    Deploying to gh-pages from @ ChunZhuo/chunzhuo.github.io@6dc9b9009c687bc97161dd402c57c432805fe53b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -494,7 +494,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/chunzhuo_zhang_cv.pdf", "_blank");
         },
       },{
         id: 'social-email',
